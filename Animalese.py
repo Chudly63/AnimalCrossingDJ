6c4e003f756e71c37abae21bf05506ec209c0f9f
Mark Board <chud63@gmail.com>
2018-09-06 19:34:08 -0400

    Attempt to use phonetic sounds instead of the names of each letter for the speaking.

--- Animalese.py
+++ Animalese.py
@@ -4,15 +4,19 @@ import time
 import wave
 import pygame
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+sound_files = ['006', '003', '020', '004', '073', '013', '026', '044', '005', '025', '027', '033', '031', '030', '042', '049', '046', '045', '007', '047', '012', '014', '032', '055', '057', '058', '059', '060', '061', '062', '063' ,'064' ,'065', '066', '067', '068']
 sounds = []
 
 frame_test = wave.open('./Voice/069.wav', 'rb').getframerate()
 
 pygame.mixer.init(frequency = frame_test, buffer=512)
 
-for x in range(69, 94):
-    fileName = './Voice/0' + str(x) +'.wav' 
+for i in range(len(alphabet)):
+    print (alphabet[i] + " : " + sound_files[i])
+
+for x in sound_files:
+    fileName = './Voice/' + x +'.wav' 
     sounds.append(pygame.mixer.Sound(fileName))
 
 def playAnimal(s):
@@ -26,6 +30,6 @@ def playAnimal(s):
         time.sleep(.05)
     print("")
 
-playAnimal('Hello there listener.   Welcome to animal crossing radio!')
-playAnimal("It's a lovely day for some music. Don't you agree?")
+playAnimal('Hello there listener.   Welcome to animal crossing radio!  ')
+playAnimal("It's currently 7:32 pm. It's a cool cloudy night outside. Here's some nice relaxing music for your evening.")
 
